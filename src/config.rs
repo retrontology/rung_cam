353f4bb8bad67d4c2ed95905eb49367870908e65
retrontology <retrontology@hotmail.com>
2023-10-09 16:38:44 -0500

    need to read more

--- src/config.rs
+++ src/config.rs
@@ -4,16 +4,21 @@ use std::path::Path;
 
 #[derive(Deserialize, PartialEq, Debug)]
 pub struct Config {
+    camera: CameraConfig,
+}
+
+#[derive(Deserialize, PartialEq, Debug)]
+pub struct CameraConfig {
     index: i32,
 }
 
 impl Config {
     
-    pub fn from_file(path: &Path) -> Result<Config, Error> {
+    pub fn from_file(path: &Path) -> Result<Config, dyn Error> {
 
         match fs::read_to_string(&path) {
             Ok(config) => serde_yaml::from_str(&config),
-            Err(error) => Error(error),
+            Err(e) => Err(e),
         }
 
     }
